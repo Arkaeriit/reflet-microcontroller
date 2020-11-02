8cba8baaa652c0ea2909d5636fbbd875913712d3
Arkaeriit <maxbouillot@gmail.com>
2020-11-02 12:17:12 +0100

    Added template registers for peripherals

---
+++ peripherals/asrm_ro_register.v
@@ -0,0 +1,36 @@
+/*-----------------------------------\
+|This is a template for the status   |
+|registers of the peripherals.       |
+|The data is stored on a single byte.|
+\-----------------------------------*/
+
+module rw_register #(
+    parameter addr_size = 16,
+    reg_addr = 0,
+    default_value = 0
+)(
+    input clk,
+    input reset,
+    input enable,
+    //system bus; note: no data_in nor data_out
+    input [addr_size-1:0] addr,
+    output reg [7:0] data_out,
+    //peripheral status
+    input [7:0] data
+    );
+
+    wire read_en = enable && reg_addr == addr;
+
+    always @ (posedge clk)
+        if(!reset)
+            data_out = 0;
+        else
+        begin
+            if(read_en)
+                data_out = data;
+            else
+                data_out = 0;
+        end
+
+endmodule
+
---
+++ peripherals/asrm_rw_register.v
@@ -0,0 +1,49 @@
+/*---------------------------------------\
+|This is a template for the configuration|
+|registers of the peripherals.           |
+|The data is stored on a single byte.    |
+\---------------------------------------*/
+
+module rw_register #(
+    parameter addr_size = 16,
+    reg_addr = 0,
+    default_value = 0
+)(
+    input clk,
+    input reset,
+    input enable,
+    //system bus
+    input [addr_size-1:0] addr,
+    input write_en,
+    input [7:0] data_in,
+    output reg [7:0] data_out,
+    //peripheral control
+    output reg [7:0] data
+    );
+
+    wire wr_en = enable && write_en && reg_addr == addr;
+    wire read_en = enable && reg_addr == addr;
+
+    always @ (posedge clk)
+        if(!reset)
+        begin
+            data_out = 0;
+            data = 0;
+        end
+        else
+        begin
+            if(wr_en)
+                data = data_in;
+            if(read_en)
+            begin
+                if(wr_en)
+                    data_out = data_in;
+                else
+                    data_out = data;
+            end
+            else
+                data_out = 0;
+        end
+
+endmodule
+
