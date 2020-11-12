9b3eb0f19e0719fc2e8aa406e1875ba7bbce46d2
Arkaeriit <maxbouillot@gmail.com>
2020-11-12 03:35:38 +0100

    Finished the interrupt controller

--- peripherals/asrm_exti.v
+++ peripherals/asrm_exti.v
@@ -23,14 +23,15 @@ module asrm_exti #(
     output [3:0] cpu_int,
     //interrupts from the peripherals
     input gpio_int_out,
-    input uart_int_out
+    input uart_int_out,
+    input timer_int_out
     );
 
-    wire using_exti = enable && addr >= base_addr && addr < base_addr + 2;
-    wire offset = addr - base_addr;
+    wire using_exti = enable && addr >= base_addr && addr < base_addr + 3;
+    wire [1:0] offset = addr - base_addr;
 
     //Controling interrupts
-    wire gpio_int, uart_int;
+    wire gpio_int, uart_int, timer_int;
     wire gpio_int_en;
     wire [1:0] gpio_int_level;
     wire [3:0] gpio_int_exti;
@@ -39,12 +40,17 @@ module asrm_exti #(
     wire [1:0] uart_int_level;
     wire [3:0] uart_int_exti;
     asrm_int_mapper uart_mapper(.enable(uart_int_en & uart_int), .level(uart_int_level), .out(uart_int_exti));
+    wire timer_int_en;
+    wire [1:0] timer_int_level;
+    wire [3:0] timer_int_exti;
+    asrm_int_mapper timer_mapper(.enable(timer_int_en & timer_int), .level(timer_int_level), .out(timer_int_exti));
 
     //gestion of status register
-    wire [7:0] int_list = {6'h0, uart_int, gpio_int}
-    reg [7:0] previous_int;
-    wire int_update = int_list & !previous_int;
-
+    wire [7:0] int_list = {5'h0, timer_int_out, uart_int_out, gpio_int_out}
+    wire int_updating = |int_list;
+    wire [7:0] status_register;
+    assign gpio_int = status_register[0];
+    assign uart_int = status_register[1];
      
     //registers
     //the first register is the enable register. bit 0 enable the gpio
@@ -52,11 +58,14 @@ module asrm_exti #(
     //the second register is used to control the interrupt numbers of
     //the gpio and uart interrupts. Bits 0 and 1 are used to controll
     //the number of the GPIO; 2 and 3 the number of the UART interrupt
+    //for timer: bit 2 of en and 4 and 5 of level
     wire [7:0] dout_enable;
     wire [7:0] dout_level;
+    wire [7:0] dout_status;
     wire [7:0] int_en;
     wire [7:0] int_level;
-    asrm_rw_register #(.addr_size(1), .reg_addr(0), .default_value(0)) reg_en(
+    assign data_out = dout_enable | dout_status | dout_level;
+    asrm_rw_register #(.addr_size(2), .reg_addr(0), .default_value(0)) reg_en(
         .clk(clk),
         .reset(reset),
         .enable(using_exti),
@@ -65,7 +74,7 @@ module asrm_exti #(
         .data_in(data_in[7:0]),
         .data_out(dout_enable),
         .data(int_en));
-    asrm_rw_register #(.addr_size(1), .reg_addr(0), .default_value(0)) reg_level(
+    asrm_rw_register #(.addr_size(2), .reg_addr(1), .default_value(0)) reg_level(
         .clk(clk),
         .reset(reset),
         .enable(using_exti),
@@ -74,12 +83,23 @@ module asrm_exti #(
         .data_in(data_in[7:0]),
         .data_out(dout_level),
         .data(int_level));
+    asrm_rwe_register #(.addr_size(2), .reg_addr(0), .default_value(0)) reg_status(
+        .clk(clk),
+        .reset(reset),
+        .enable(using_exti),
+        .addr(offset),
+        .write_en(write_en),
+        .data_in(data_in[7:0]),
+        .data_out(dout_status),
+        .data(status_register)
+        .data_override(status_register & int_list),
+        .override(int_updating));
     assign gpio_int_en = int_en[0];
     assign gpio_int_level = int_level[1:0];
     assign uart_int_en = int_en[1];
     assign uart_int_level = int_level[3:2];
-
-
+    assign timer_int_en = int_en[3];
+    assign timer_int_level = int_level[5:4];
 
 endmodule
 
