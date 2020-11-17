cd9e8dcee630e7d7021e051891d94a55e061ad5b
Arkaeriit <maxbouillot@gmail.com>
2020-11-17 11:23:44 +0100

    Improved the pwm. It is still a bit off in its forst cyclt ,tho .

--- peripherals/reflet_pwm.v
+++ peripherals/reflet_pwm.v
@@ -14,26 +14,29 @@ module reflet_pwm_pwm (
     reg out_normal;
     wire update;
 
+    wire using_count_off = duty_cycle > 1 && duty_cycle < max;
+    wire using_count_base = using_count_off | duty_cycle == 1;
+
     wire base_freq, switch_off;
     reflet_counter count_base (
         .clk(clk),
         .reset(reset & !update),
-        .enable(1'b1),
+        .enable(using_count_base),
         .max({24'h0, max}),
         .out(base_freq));
     reflet_counter count_off (
         .clk(clk),
-        .reset(reset & !update),
-        .enable(out),
-        .max({24'h0, duty_cycle}),
+        .reset(reset & !update & !base_freq), //This counter should start a clk cycle after the other counter, it should thus be reset by uate_late
+        .enable(using_count_off & out_normal),
+        .max({24'h0, duty_cycle - 1}), //As the counter is reset each cycles, we need to put a -1 because this counter is a clock cycle late
         .out(switch_off));
 
     always @ (posedge clk)
-        if(!reset | update)
+        if(!reset)
             out_normal = 0;
         else
         begin
-            if(base_freq)
+            if(base_freq | update)
                 out_normal = 1;
             else if(switch_off)
                 out_normal = 0;
@@ -47,7 +50,8 @@ module reflet_pwm_pwm (
 
     assign out = ( duty_cycle == 0 ? 0 :
                     ( duty_cycle == 1 ? base_freq :
-                       out_normal));
+                       ( duty_cycle >= max ? 1 :
+                          out_normal)));
 
 endmodule
 
--- simu/reflet_8bit_ctrl_tb.v
+++ simu/reflet_8bit_ctrl_tb.v
@@ -5,10 +5,11 @@ module reflet_8bit_ctrl_tb ();
     always #1 clk = !clk;
     wire tx;
     reg rx = 1;
+    reg reset_in = 1;
 
     reflet_8bit_ctrl_with_rom ctrl (
         .clk(clk),
-        .reset_in(1'b1),
+        .reset_in(reset_in),
         .debug(),
         .quit(),
         .gpi(16'b0),
@@ -19,6 +20,14 @@ module reflet_8bit_ctrl_tb ();
     initial
     begin
         #1000;
+        rx = 0;
+        #200;
+        rx = 1;
+        #2000;
+        reset_in = 0;
+        #10;
+        reset_in = 1;
+        #100;
         rx = 0;
         #200;
         rx = 1;
