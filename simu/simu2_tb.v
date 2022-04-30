96151e1203b2c6115a782349509ae70aaf21985a
Maxime Bouillot <maxime.bouillot@withings.com>
2022-04-30 23:56:21 +0200

    Used peripheral module for simu2.

--- simu/simu2_tb.v
+++ simu/simu2_tb.v
@@ -16,21 +16,38 @@ module simu2();
     wire rx = 1;
     wire tx;
 
-    //modules
-    wire [7:0] data_out_uart;
+    // Peripheral
+    wire [7:0] dout_periph;
+    wire [7:0] din_periph = (addr[0] ? data_out_cpu[15:8] : data_out_cpu[7:0]);
+    wire [15:0] dout_periph_shift = (addr[0] ? {dout_periph, 8'h0} : {8'h0, dout_periph});
+    reflet_peripheral #(
+        .wordsize(16), 
+        .base_addr_size(15), 
+        .base_addr(15'h7F00), 
+        .clk_freq(96000),
+        .enable_exti(0),
+        .enable_gpio(0),
+        .enable_timer(0),
+        .enable_timer2(0),
+        .enable_uart(1),
+        .enable_pwm(0),
+        .enable_segments(0),
+        .enable_power_manager(0),
+        .enable_synth(0)
+    ) periph (
+        .clk(clk),
+        .reset(reset),
+        .enable(addr[15]),
+        .addr(addr[14:0]),
+        .data_in(din_periph),
+        .data_out(dout_periph),
+        .write_en(write_en),
+        .rx(rx),
+        .tx(tx));
+
+    // Memory
     wire [15:0] data_out_rom;
     wire [15:0] data_out_ram;
-    wire [7:0] din_periph = (addr[0] ? data_out_cpu[15:8] : data_out_cpu[7:0]);
-    reflet_uart #(.base_addr_size(15), .base_addr(15'h7F16), .clk_freq(96000)) uart (
-        .clk(clk), 
-        .reset(reset), 
-        .enable(addr[15]), 
-        .addr(addr[14:0]), 
-        .write_en(write_en), 
-        .data_in(din_periph), 
-        .data_out(data_out_uart), 
-        .rx(rx), 
-        .tx(tx));
     rom2 rom(
         .clk(clk), 
         .enable(!addr[15]), 
@@ -44,7 +61,7 @@ module simu2();
         .data_in(data_out_cpu), 
         .write_en(write_en), 
         .data_out(data_out_ram));
-    assign data_in_cpu = data_out_rom | {8'h0, data_out_uart} | data_out_ram;
+    assign data_in_cpu = data_out_rom | dout_periph_shift | data_out_ram;
     reflet_cpu #(.wordsize(16)) cpu(
         .clk(clk), 
         .reset(reset), 
@@ -65,7 +82,7 @@ module simu2();
             $dumpvars(0, cpu.registers[i]);
         #10;
         reset = 1;
-        #25000;
+        #40000;
         $finish;
     end
 
